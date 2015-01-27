5a049a13ca6518a442b2b980bfbcf3540fa28176
Guinsly Mondesir <guinslym @gmail.com>
2015-01-27 00:31:23 -0500

    New 2015 Layout with particle.js

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ activate :blog do |blog|
   # Matcher for blog source files
    blog.sources = "{year}-{month}-{day}-{title}.html"
    blog.taglink = "tags/{tag}.html"
-   blog.layout = "layout"
+   blog.layout = "layout"#the MAIN project layout
    blog.summary_separator = /(READMORE)/
    blog.summary_length = 150
    blog.year_link = "{year}.html"
